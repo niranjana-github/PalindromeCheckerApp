75f6d9f10fd8fa60f87fa346e0ee3953f689ec5e
niranjana-github <niranjanak1906@gmail.com>
2026-03-13 09:59:29 +0530

    Update UC1 file

--- app/src/PalindromeChckerApp.java
+++ app/src/PalindromeChckerApp.java
@@ -3,5 +3,5 @@ public class PalindromeChckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
-    }
+}
 }
